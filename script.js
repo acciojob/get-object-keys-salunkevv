ea1cbc74df09592d7307871a64880fc687b58da0
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-12-18 23:01:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let student={
+	name:"vivek"
+}
+Object.prototype.getKeys=function()
+{
+	let arr=[];
+	for(let i in this)
+		{
+			arr.push(i);
+		}
+	return arr;
+}
